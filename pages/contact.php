af248344aabc0597c16ae7aff0f58e60948d3a60
simplon <rudy__10@hotmail.fr>
2017-01-08 20:59:11 +0100

    ajout formulaire

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,24 @@
-rudy__10@hotmail.fr
+<form action="save.php" method="post">
+
+
+<div>
+	
+<label for="name">Nom</label>
+<input type="text" value="Rudy" name="name" id="name">
+</div>
+
+<div>
+<label for="message"></label>
+
+<textarea name="" id="" cols="30" rows="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quae ex excepturi animi, impedit ut eveniet, velit asperiores ratione fugit, enim culpa tempore dignissimos magni vero cupiditate cum. Consequuntur, deleniti.</textarea>
+</div>
+<button>Cliquez pour envoyer</button>
+
+
+
+
+
+
+
+
+</form>
